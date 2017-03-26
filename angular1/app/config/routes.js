fb639fa7ddf8720a668077a60af9140d6af920d6
Igor Henrique de Araújo <igor.henrique.araujo@gmail.com>
2017-03-26 20:35:22 -0300

    Dando continuidade nas aulas, finalização do Dashboard em comunicação com o backend e implementação de cross-origin no backend

--- angular1/app/config/routes.js
+++ angular1/app/config/routes.js
@@ -1,15 +1,18 @@
-angular.module('cicloDePagamentos').config([
-  '$stateProvider',
-  '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider){
-    $stateProvider.state('dashboard', {
-      url: "/dashboard",
-      templateUrl: "dashboard/dashboard.html"
-    }).state('billingCycle', {
-      url: "/billingCycles",
-      templateUrl: "billingCycle/tabs.html "
-    })
+(function(){
+  angular.module('cicloDePagamentos').config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider){
+      $stateProvider.state('dashboard', {
+        url: "/dashboard",
+        templateUrl: "dashboard/dashboard.html"
+      }).state('billingCycle', {
+        url: "/billingCycles",
+        templateUrl: "billingCycle/tabs.html "
+      })
 
-    $urlRouterProvider.otherwise('/dashboard')
-  }
-])
+      $urlRouterProvider.otherwise('/dashboard')
+    }
+  ])
+
+})()
