6b6002dba14994057fc532004f29c99adac74532
Igor Henrique de Araújo <igor.henrique.araujo@gmail.com>
2017-03-27 16:59:47 -0300

    Finalizando siglo de CRUD para formulário inicial do ciclo de pagamentos

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -27,6 +27,36 @@
       })
     }
 
+    vm.showTabUpdate = function(billingCycle) {
+      vm.billingCycle = billingCycle
+      tabs.show(vm, {tabUpdate: true})
+    }
+
+    vm.showTabDelete = function(billingCycle) {
+      vm.billingCycle = billingCycle
+      tabs.show(vm, {tabDelete: true})
+    }
+
+    vm.update = function() {
+      const updateUrl = `${url}/${vm.billingCycle._id}`
+      $http.put(updateUrl, vm.billingCycle).then(function(response){
+        vm.refresh()
+        msgs.addSuccess('Operação realizada com sucesso!')
+      }).catch(function(response){
+        msgs.addError(response.data.errors)
+      })
+    }
+
+    vm.delete = function() {
+      const deleteUrl = `${url}/${vm.billingCycle._id}`
+      $http.delete(deleteUrl, vm.billingCycle).then(function(response){
+        vm.refresh()
+        msgs.addSuccess('Operação realizada com sucesso!')
+      }).catch(function(response){
+        msgs.addError(response.data.errors)
+      })
+    }
+
     vm.refresh()
 
   }
