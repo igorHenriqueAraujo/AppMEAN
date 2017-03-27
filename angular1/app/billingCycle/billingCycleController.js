71a7920a75aa7113aa5f80495ba39ef75d026438
Igor Henrique de Araújo <igor.henrique.araujo@gmail.com>
2017-03-27 15:49:49 -0300

    Avançando nas aulas, implementação das mensagens e da versão inicial da tela de listagem

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,18 +1,32 @@
 (function(){
   angular.module('cicloDePagamentos').controller('BillingCycleController', [
     '$http',
+    'msgs',
     BillingCycleController
   ])
 
-  function BillingCycleController($http){
+  function BillingCycleController($http, msgs){
     const vm = this
+    const url = 'http://localhost:3003/api/billingCycles'
 
-    vm.create = function() {
-      const url = 'http://localhost:3003/api/billingCycles'
-      $http.post(url, vm.billingCycle).then(function(response){
+    vm.refresh = function() {
+      $http.get(url).then(function(response){
         vm.billingCycle = {}
+        vm.billingCycles = response.data
       })
     }
+
+    vm.create = function() {
+      $http.post(url, vm.billingCycle).then(function(response){
+        vm.refresh()
+        msgs.addSuccess('Operação realizada com sucesso!!!')
+      }).catch(function(response){
+        msgs.addError(response.data.errors)
+      })
+    }
+
+    vm.refresh()
+
   }
 
 })()
